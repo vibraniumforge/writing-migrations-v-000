0d17d45a4f7dfe03cb5ee08b5dd5738b5645075c
Greg Morgan <gregm9999@gmail.com>
2018-11-23 21:45:43 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,15 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
-  def change
-  create_table :artists do |t|
-    t.string :name
-    t.string :genre
-    t.integer :age
-    t.string :hometown
-  end
-end
 
 def change
    add_column :artists, :name, :string
  end
-  
+
 end
